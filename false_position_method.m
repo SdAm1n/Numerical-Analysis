09af91b7ed71f19c507a953678e2dc35bdf9c38f
Md. Sadikul Amin Sadman <anarchst49@gmail.com>
2024-01-29 11:48:59 +0600

    mod falsi method

--- false_position_method.m
+++ false_position_method.m
@@ -1,9 +1,13 @@
 function root = false_position_method()
-  a = 2;
-  b = 3;
+  a = -1;
+  b = -3;
   max_iter = 10;
-  fa = a ^ 3 - 2 * a - 5;
-  fb = b ^ 3 - 2 * b - 5;
+  f = @(x) (3 * x ^ 4) - (2 * x ^ 2) - (2 * x) - 8;
+  %f = @(x) x ^ 3 - 2 * x - 5;
+  fa = f(a);
+  fb = f(b);
+  %fa = a ^ 3 - 2 * a - 5;
+  %fb = b ^ 3 - 2 * b - 5;
   if (fa * fb > 0)
     return;
   endif
@@ -11,7 +15,8 @@ function root = false_position_method()
   iter = 0;
   while (iter < max_iter)
     c = (a * fb - b * fa)/(fb - fa);
-    fc = c ^ 3 - 2 * c - 5;
+    %fc = c ^ 3 - 2 * c - 5;
+    fc = f(c);
     if (fc == 0)
       root = c;
     else if(fa * fb < 0)
@@ -27,4 +32,5 @@ function root = false_position_method()
   root = (a + b) / 2;
   fprintf('Iter: %d = Root: %f\n',iter,root);
 end
+
 
